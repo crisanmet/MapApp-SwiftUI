fa13ae87360367dc69e13d9cf48be0adf9262368
Cristian Sancricca <cristiansancricca@gmail.com>
2022-07-18 13:13:37 -0300

    Initial Commit

---
+++ MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by Cristian Sancricca on 18/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
