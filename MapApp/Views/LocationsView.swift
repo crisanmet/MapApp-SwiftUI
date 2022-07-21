dfc4860991c9380d1c529aa00ddff299383e8d3b
Cristian Sancricca <cristiansancricca@gmail.com>
2022-07-21 01:06:22 -0300

    initial commit

---
+++ MapApp/Views/LocationsView.swift
@@ -0,0 +1,97 @@
+//
+//  LocationsView.swift
+//  MapApp
+//
+//  Created by Cristian Sancricca on 18/07/2022.
+//
+
+import SwiftUI
+import MapKit
+
+struct LocationsView: View {
+    
+    @EnvironmentObject private var viewModel: LocationsViewModel
+    
+    var body: some View {
+        ZStack {
+            mapLayer
+                .ignoresSafeArea()
+            VStack(spacing: 0) {
+                header
+                    .padding()
+                
+                Spacer()
+                
+                locationsPreviewStack
+            }
+        }
+        .sheet(item: $viewModel.sheetLocation, onDismiss: nil) { location in
+            LocationsDetailView(location: location)
+        }
+    }
+}
+
+struct LocationsView_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationsView()
+            .environmentObject(LocationsViewModel())
+    }
+}
+
+extension LocationsView {
+    
+    private var header: some View {
+        VStack {
+            Button(action: viewModel.toggleLocationsList) {
+                Text(viewModel.mapLocation.name + ", " + viewModel.mapLocation.cityName)
+                    .font(.title2)
+                    .fontWeight(.black)
+                    .foregroundColor(.primary)
+                    .frame(height: 55)
+                    .frame(maxWidth: .infinity)
+                    .animation(.none, value: viewModel.mapLocation)
+                    .overlay(alignment: .leading) {
+                        Image(systemName: "arrow.down")
+                            .font(.headline)
+                            .foregroundColor(.primary)
+                            .padding()
+                            .rotationEffect(Angle(degrees: viewModel.showLocationsList ? 180 : 0))
+                    }
+            }
+  
+            if viewModel.showLocationsList {
+                LocationsListView()
+            }
+        }
+        .background(.thickMaterial)
+        .cornerRadius(10)
+        .shadow(color: Color.black.opacity(0.3), radius: 20, x: 0, y: 15)
+        
+    }
+    
+    private var mapLayer: some View {
+        Map(coordinateRegion: $viewModel.mapRegion, annotationItems: viewModel.locations, annotationContent: { location in
+            MapAnnotation(coordinate: location.coordinates) {
+                LocationMapAnnotationView()
+                    .scaleEffect(viewModel.mapLocation == location ? 1 : 0.7)
+                    .shadow(radius: 10)
+                    .onTapGesture {
+                        viewModel.showNextLocation(location: location)
+                    }
+            }
+        })
+    }
+    
+    private var locationsPreviewStack: some View {
+        ZStack {
+            ForEach(viewModel.locations) { location in
+                if viewModel.mapLocation == location {
+                    LocationPreview(location: location)
+                        .shadow(color: Color.black.opacity(0.3), radius: 20)
+                        .padding()
+                        .transition(.asymmetric(insertion: .move(edge: .trailing), removal: .move(edge: .leading)))
+                }
+            }
+        }
+    }
+}
