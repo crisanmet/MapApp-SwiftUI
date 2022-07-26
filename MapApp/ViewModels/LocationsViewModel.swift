3172ae66e18632a89044db4ff4eb45db920c6c06
Cristian Sancricca <cristiansancricca@gmail.com>
2022-07-25 23:52:54 -0300

    test

--- MapApp/ViewModels/LocationsViewModel.swift
+++ MapApp/ViewModels/LocationsViewModel.swift
@@ -17,6 +17,7 @@ class LocationsViewModel: ObservableObject {
             updateMapRegion(location: mapLocation)
         }
     }
+    
     @Published var mapRegion: MKCoordinateRegion = MKCoordinateRegion()
     let mapSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
     
